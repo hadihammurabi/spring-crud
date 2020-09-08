e0fe6b22b3a1c1e79b038c062e2adc172e8ca386
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-09-08 08:21:36 +0700

    service add get user by id

--- src/main/java/com/hadihammurabi/belajar/service/UserService.java
+++ src/main/java/com/hadihammurabi/belajar/service/UserService.java
@@ -1,6 +1,7 @@
 package com.hadihammurabi.belajar.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,4 +25,9 @@ public class UserService {
   public List<User> getUsers() {
     return this.userRepository.findAll();
   }
+
+  public User getUser(UUID id) {
+    return this.userRepository.findById(id);
+  }
+
 }
