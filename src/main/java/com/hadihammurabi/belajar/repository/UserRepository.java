1b42e39015a322e22d638ed4ee43e6db43f58dbb
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-09-08 08:21:06 +0700

    repo add find by id

--- src/main/java/com/hadihammurabi/belajar/repository/UserRepository.java
+++ src/main/java/com/hadihammurabi/belajar/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.hadihammurabi.belajar.repository;
 
+import java.util.List;
+import java.util.UUID;
+
 import com.hadihammurabi.belajar.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+  User findById(UUID id);
+}
